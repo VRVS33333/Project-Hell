b1b327f49b8bbce449c20a403b113d6b9bd09fa1
unknown <vinayakrevisanker@gmail.com>
2025-09-08 14:39:54 -0500

    changed code for astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless';
+import solid from '@astrojs/solid-js';
 // https://astro.build/config
 export default defineConfig({
     output: 'server',
-    adapter: node({ mode: 'standalone' }),
+    adapter: vercel({}),
+    integrations: [solid()],
 });
